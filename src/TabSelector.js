da3820f4ee6d56d64d5cab943720ac4d819117e3
Numissa11 <aline_sinclaire@hotmail.fr>
2020-05-10 19:58:00 +0100

    second

---
+++ src/TabSelector.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class TabSelector extends Component {
+
+  render() {
+    const { activeId } = this.props;
+    console.log('activeId :', activeId)
+    return (
+      <div className="TabSelector">
+        <button
+          id="home"
+          onClick={this.props.handleChangeTab}
+          className={activeId === 'home' ? 'active' : ''}
+        >
+          Home
+         
+        </button>
+
+        <button
+          id="about"
+          onClick={this.props.handleChangeTab}
+          className={activeId === 'about' ? 'active' : ''}
+        >
+          About
+        </button>
+        <button
+          id="contact"
+          onClick={this.props.handleChangeTab}
+          className={activeId === 'contact' ? 'active' : ''}
+        >
+          Contact
+        </button>
+      </div>
+    );
+  }
+}
+
+export default TabSelector;
